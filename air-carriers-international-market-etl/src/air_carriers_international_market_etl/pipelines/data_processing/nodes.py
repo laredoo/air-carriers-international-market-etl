6b54fb1da2d8223f2863fb5b615d3b645821f270
Lucas Laredo <luckrocaabh@gmail.com>
2025-06-27 00:52:53 -0300

    feat: All pipelines implemented

--- air-carriers-international-market-etl/src/air_carriers_international_market_etl/pipelines/data_processing/nodes.py
+++ air-carriers-international-market-etl/src/air_carriers_international_market_etl/pipelines/data_processing/nodes.py
@@ -3,8 +3,9 @@ import pandas as pd
 logger = __import__("kedro").get_logger(__name__)
 
 
-def create_flight_company_dimension(
+def create_class_dimension(
     market_data_2024: pd.DataFrame,
+    service_class: pd.DataFrame,
 ) -> pd.DataFrame:
     """Combines all data to create a flight company dimension table.
 
@@ -16,15 +17,19 @@ def create_flight_company_dimension(
     """
     logger.info("Creating flight company dimension table")
 
-    rated_shuttles = airline_id.merge(
-        carrier_history, left_on="id", right_on="shuttle_id"
-    )
-    rated_shuttles = rated_shuttles.drop("id", axis=1)
-    model_input_table = rated_shuttles.merge(
-        carrier_group_new, left_on="company_id", right_on="id"
-    )
-    model_input_table = model_input_table.dropna()
-    return model_input_table
+    class_dimension = pd.DataFrame()
+
+    class_dimension = market_data_2024[["CLASS"]].drop_duplicates()
+    class_dimension = class_dimension.merge(
+        service_class,
+        left_on="CLASS",
+        right_on="Code",
+        how="left",
+    ).drop(columns=["CLASS"])
+
+    class_dimension["id_class"] = class_dimension.reset_index().index + 1
+
+    return class_dimension
 
 
 def create_airport_dimension(
@@ -87,13 +92,11 @@ def create_distance_dimension(
     """
     logger.info("Creating distance dimension table")
 
-    distance_dimension = distance_group_500.rename(
-        columns={"id_distance_group": "Code", "ds_faixa_distancia": "Description"},
-    )
+    distance_dimension = pd.DataFrame()
 
-    distance_dimension = distance_dimension.insert(
-        0, "id_distancia_sk", range(1, len(distance_dimension) + 1)
-    )
+    # not implemented Faixa_Descricao
+    distance_dimension = market_data_2024[["DISTANCE_GROUP"]].drop_duplicates()
+    distance_dimension["id_distancia"] = distance_dimension.reset_index().index + 1
 
     return distance_dimension
 
@@ -151,8 +154,6 @@ def create_operator_dimension(
         .reset_index(drop=True)
     )
     operator_dimension["id_operadora"] = operator_dimension.index + 1
-
-    logger.info("Operator dimension table created successfully")
 
     return operator_dimension
 
--- air-carriers-international-market-etl/src/air_carriers_international_market_etl/pipelines/data_processing/pipeline.py
+++ air-carriers-international-market-etl/src/air_carriers_international_market_etl/pipelines/data_processing/pipeline.py
@@ -2,9 +2,9 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 from .nodes import (
     create_airport_dimension,
+    create_class_dimension,
     create_company_dimension,
     create_distance_dimension,
-    create_flight_company_dimension,
     create_operator_dimension,
     create_time_dimension,
 )
@@ -14,10 +14,10 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=create_flight_company_dimension,
-                inputs=["market_data_2024"],
-                outputs="flight_company_dimension",
-                name="create_flight_company_dimension_node",
+                func=create_class_dimension,
+                inputs=["market_data_2024", "service_class"],
+                outputs="class_dimension",
+                name="create_class_dimension_node",
             ),
             node(
                 func=create_airport_dimension,
